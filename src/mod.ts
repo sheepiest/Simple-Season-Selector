8e3ce5a8de17c3be29b9d28793d431a0f4d4d84e
sheepy <39248026+sheepiest@users.noreply.github.com>
2024-07-20 13:51:57 +0100

    1.1.0
    code un-mess-ified, no new functionality

--- src/mod.ts
+++ src/mod.ts
@@ -9,84 +9,69 @@ import { ILogger } from "@spt/models/spt/utils/ILogger";
 import { IWeatherConfig } from "@spt/models/spt/config/IWeatherConfig";
 
 class Mod implements IPostDBLoadMod
-
 {
-    //lets the user type out the season name instead of having to mess with numbers
-    private mappings: { [key: string]: number };
-    private logoutputMappings: { [key: string]: string };
-    private modName: "[Simple Season Selector]"
 
-    constructor() 
-    {
-        this.mappings = {
-            "summer": 0,
-            "autumn": 1,
-            "winter": 2,
-            "spring": 3,
-            "storm": 4
-        };
-        this.logoutputMappings={
-            0: "Summer",
-            1: "Autumn",
-            2: "Winter",
-            3: "Spring",
-            4: "Storm",
-            null: "Auto"
-        }
-    }
-    //turns seasons into numbers. probably overkill, but making it more streamlined would require knowledge of what is going on. also, means Auto gets turned into null
-    public mapStringToNumber(input: string): number | null 
-    {
-        const lowerCaseInput = input.toLowerCase();
-        if (Object.prototype.hasOwnProperty.call(this.mappings, lowerCaseInput)) 
-            
-        {
-            return this.mappings[lowerCaseInput];
-        }
-        else 
-        {
-            return null;
-        }
-    }
-    //turns a number back into a displayable season for the log
-    private mapAnyToString(input: any): string | null 
-    {
-        if (Object.prototype.hasOwnProperty.call(this.logoutputMappings, input)) 
-            
-        {
-            return this.logoutputMappings[input];
-        }
-        else 
-        {
-            return null;
-        }
-    }
-
+    private modName = "[Simple Season Selector]"
 
     public postDBLoad(container: DependencyContainer): void
     {
-        //base code graciously provided by AcidPhantasm
+        // code graciously provided by AcidPhantasm
         const vfs = container.resolve<VFS>("VFS");
         const modConfigJsonC = jsonc.parse(vfs.readFile(path.resolve(__dirname, "../config/config.jsonc")));
         const configServer = container.resolve<ConfigServer>("ConfigServer");
         const weatherConfig : IWeatherConfig = configServer.getConfig(ConfigTypes.WEATHER);
         const logger = container.resolve<ILogger>("WinstonLogger");
 
-        //grabbing the config value, turning it into a number
-        const selectedseasonNumber = this.mapStringToNumber(modConfigJsonC.SelectedSeason)
-        
-        //setting the db season to the number
-        weatherConfig.overrideSeason = selectedseasonNumber
-        
-        //checks what overrideSeason is for outputting to the console
-        const loggedSeason = this.mapAnyToString(weatherConfig.overrideSeason)
+        // logger.warning(`${this.modName} Debug: modConfigJsonC = ${modConfigJsonC.SelectedSeason}`) // debug
 
-        //spits out the selected season string in the console in case the user forgor, also looks nice
-        logger.success(`${this.modName} Selected season: ${loggedSeason}`)
+        const seasonsTuple:[number, string][] = [[0, "Summer"],[1, "Autumn"],[2, "Winter"],[3, "Spring"],[4, "Storm"]] // seasons tuple
+        /*
+        "summer": 0,
+        "autumn": 1,
+        "winter": 2,
+        "spring": 3,
+        "storm": 4
+        */
 
-        //debug logs to see if it did the thing properly
-        //logger.info(`${selectedseasonNumber}`)
-        //logger.info(`${weatherConfig.overrideSeason}`)
+        // config input sanitiser
+        function inputSanitiser(input:string) : string
+        {
+            return input && input[0].toUpperCase() + input.slice(1).toLowerCase()
+        }
+
+        const sanitisedSelectedSeason: string = inputSanitiser(modConfigJsonC.SelectedSeason) // pulls and sanitises config value
+
+        // logger.warning(`${this.modName} Debug: sanitisedSelectedSeason = ${sanitisedSelectedSeason}`) // debug
+
+        weatherConfig.overrideSeason = null // preinitialises the season to null
+
+        // setting the db season
+
+        if (sanitisedSelectedSeason === "Auto" || null)
+        {
+            logger.success(`${this.modName} Season Selected: Auto`) // yes, Auto is just null wearing a fancy hat
+            
+        } 
+        else
+        {
+            for (let i = 0; i < seasonsTuple.length; i++) // probably should have been a switch, however, what if there were 100 seasons? wait
+            {
+                if (sanitisedSelectedSeason === seasonsTuple[i][1]) // if @sanitisedSelectedSeason matches string from array, spit out number
+                {
+                    weatherConfig.overrideSeason = seasonsTuple[i][0]
+                    logger.success(`${this.modName} Season Selected: ${seasonsTuple[i][1]}`)
+                    break
+                }   
+            }
+        }
+
+        // logger.warning(`${this.modName} Debug: overrideSeason = ${weatherConfig.overrideSeason}`) // debug
+        
+        if (sanitisedSelectedSeason !== "Auto" && weatherConfig.overrideSeason === null) 
+        {
+            logger.warning(`${this.modName} Did you misspell: "${modConfigJsonC.SelectedSeason}"? Defaulting to Auto`) // the perks of being a Helper is kinda knowing what issues people will have
+        }
+    
     }
 }
 
